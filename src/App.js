7e62974fc9dbefebe82285345fa38b725603891e
Kelvin Chan <33172485+MyAngelNeptune@users.noreply.github.com>
2019-05-31 14:05:11 -0700

    Updated favi/new Home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import nep from './Components/Images/anep3.png';
+import Home from './Components/JS/Home.js';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={nep} className="App-logo" alt="logo" />
-        <p>
-          Hi!!!!
-        </p>
+        <Home />
       </header>
     </div>
   );
---
+++ src/Components/JS/Navbar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+
+class Navbar extends Component {
+	render(){
+		return(
+			<Router>
+
+			</Router>
+		)
+	}
+
+}
+
+export default Navbar;
