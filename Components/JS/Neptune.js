cf3dc4f7269cae143370accc4b971c63db230a5e
Kelvin Chan <33172485+MyAngelNeptune@users.noreply.github.com>
2019-05-31 22:48:17 -0700

    Home screen

---
+++ Components/JS/Neptune.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Neptune extends Component {
+  render() {
+    return (
+        <div className='Home'>
+        	<div className='boxCenter'>
+						<h1 className='Me'>
+
+					 	</h1>
+						<h2> hi </h2>
+					</div>
+        </div>
+    );
+  }
+}
+
+export default Neptune;
